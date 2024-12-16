5027e84d58201a31fd2157c608930d0aee816661
Sebastian Flores <sebastian.flores@u-planner.com>
2024-12-15 22:05:36 -0300

    more photos

--- photo_carousel.py
+++ photo_carousel.py
@@ -1,24 +1,12 @@
 import streamlit as st
 from streamlit_carousel import carousel
 from data import content_dict, button_dict
+from glob import glob
 
-test_items = [
-    dict(
-        title="",
-        text="",
-        img="carousel/pyschool_00.jpg",
-    ),
-    dict(
-        title="",
-        text="",
-        img="carousel/pyschool_01.jpg",
-    ),
-    dict(
-        title="",
-        text="",
-        img="carousel/pyschool_02.jpg",
-    ),
-]
+# Dynamically generate the carousel items
+photo_list = sorted(glob("carousel/*.jpg") + glob("carousel/*.png") + glob("carousel/*.jpeg"))
+
+test_items = [ {"title": "", "text": "", "img": photo} for photo in photo_list ]
 
 # Using https://github.com/thomasbs17/streamlit-contributions/tree/master/bootstrap_carousel
 keyword = "photos"
